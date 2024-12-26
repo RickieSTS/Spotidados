2126bb5df075bccb248d411bcdc03279066adf2d
RickieSTS <sysgames.77@gmail.com>
2024-12-26 16:30:52 +0000

    working on g2

---
+++ src/Components/g2/g2.jsx
@@ -0,0 +1,9 @@
+import dados from "../../spotify_data_history.json";
+
+export function G2() {
+  const count = new Set(dados.map((e) => e.master_metadata_track_name));
+
+  return <>
+  {count.size}
+  </>;
+}
