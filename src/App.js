839729f932518b231bd2897c0202c3b8ab6fb58a
RickieSTS <sysgames.77@gmail.com>
2024-12-26 14:58:05 +0000

    Added grid to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-
-import './App.css';
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import NestedList from './Components/teste/teste';
-
+import "./App.css";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid2";
+import { styled } from "@mui/material/styles";
 
 function App() {
   return (
     <div className="App">
-     <Button variant="contained">Hello world</Button>
-     <NestedList />
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="xxl">
+          <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+            <Grid container spacing={2} columns={16}>
+            <Grid size={{xs:16}}>
+               Header
+              </Grid>
+              <Grid size={{ xs: 6, md: 8 }}>
+               1
+              </Grid>
+              <Grid size={{ xs: 6, md: 8 }}>
+                2
+              </Grid>
+              <Grid size={{ xs: 6, md: 4 }}>
+               3
+              </Grid>
+              <Grid size={{ xs: 6, md: 8 }}>
+              4 
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
