bf870d8f6059dfa3dedb2ee1a69bba2e6005b33a
RickieSTS <sysgames.77@gmail.com>
2024-12-29 19:28:18 +0000

    Fixed Logic on G1 to account for podcasts

--- src/Components/PlaysTotais/PlaysTotais.jsx
+++ src/Components/PlaysTotais/PlaysTotais.jsx
@@ -1,30 +1,33 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography } from "@mui/material";
 //G1) Ver quantas plays no total.
 
-
-function PlaysTotais({dados}) {
-
-
- return (
-   <Card sx={{ 
-    bgcolor: '#666666', 
-    border: '2px solid #808080', 
-    borderRadius: '15px',
-    color: 'white',
-    minWidth: 200,
-    margin: 2
-   }}>
-     <CardContent>
-       <Typography variant="h6" gutterBottom>
-         Total de Plays
-       </Typography>
-       <Typography variant="h5">
-         
-         Plays: {dados.length}
-       </Typography>
-     </CardContent>
-   </Card>
- );
+function PlaysTotais({ dados }) {
+  //Verifica se a musica não é NULL para contar, se for NULL é um podcast
+  const playsTotais = dados.reduce((acc, cur) => {
+      if(cur.master_metadata_track_name){
+        acc++
+      }
+    return acc;
+  }, 0);
+  return (
+    <Card
+      sx={{
+        bgcolor: "#666666",
+        border: "2px solid #808080",
+        borderRadius: "15px",
+        color: "white",
+        minWidth: 200,
+        margin: 2,
+      }}
+    >
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          Total de Plays
+        </Typography>
+        <Typography variant="h5">Plays: {playsTotais}</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default PlaysTotais;
